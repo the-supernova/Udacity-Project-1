576200bf337d0ac351b48452ef15422392e4d83f
Abhijit Biswal <biswalabhijit04@gmail.com>
2021-04-10 23:48:35 +0530

    Change log level to DEBUG

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,7 +67,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            app.logger.info('Login was unsuccessful')
+            app.logger.warning('Login was unsuccessful')
             return redirect(url_for('login'))
         app.logger.info('Login was successful')
         login_user(user, remember=form.remember_me.data)
